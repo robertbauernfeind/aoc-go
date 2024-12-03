875c34156e3a95c2890e5c5db79d818cd8ea8d05
Robert Bauernfeind <r.bauernfeind@braun.at>
2024-12-03 09:42:03 +0100

    complete day 2 star 2, refactor day 2 star 1

--- 2024/02/code.go
+++ 2024/02/code.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"math"
 	"strconv"
 	"strings"
 
@@ -20,19 +19,60 @@ func main() {
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
 	reports := strings.Split(input, "\n")
-	safe := 0
+	safeCtr := 0
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
-		return 12
+		for _, report := range reports {
+			levels := stringSliceToIntSlice(strings.Split(report, " "))
+			if safe(levels) || safeWithRemoving(levels) {
+				safeCtr++
+			}
+		}
+		return safeCtr
 	}
 	// solve part 1 here
 	for _, report := range reports {
 		levels := stringSliceToIntSlice(strings.Split(report, " "))
-		if (isIncreasing(levels) || isDecreasing(levels)) && diffIsSave(levels) {
-			safe++
+		if safe(levels) {
+			safeCtr++
 		}
 	}
-	return safe
+	return safeCtr
+}
+
+func safeWithRemoving(levels []int) bool {
+	for i := range levels {
+		nl := append([]int{}, levels[:i]...)
+		nl = append(nl, levels[i+1:]...)
+
+		if safe(nl) {
+			return true
+		}
+	}
+	return false
+}
+
+func safe(levels []int) bool {
+	// safe when either asscendin or descending
+	// and diff between levels is 1 or 2
+	asc := levels[0] < levels[1]
+	for i := 1; i < len(levels); i++ {
+		diff := levels[i] - levels[i-1]
+		// if diff is less then 0, levels are descending
+		// if diff is equal to 0 its neither asc or desc so its false
+		if diff > 0 != asc {
+			return false
+		}
+
+		if diff < 0 {
+			diff *= -1
+		}
+
+		if diff < 1 || diff > 3 {
+			return false
+		}
+	}
+	return true
 }
 
 func stringSliceToIntSlice(s []string) []int {
@@ -42,42 +82,4 @@ func stringSliceToIntSlice(s []string) []int {
 		r = append(r, j)
 	}
 	return r
-}
-
-func isIncreasing(levels []int) bool {
-	for i := 0; i < len(levels)-1; i++ {
-		lvl1, lvl11 := levels[i], levels[i+1]
-		if lvl1 >= lvl11 {
-			return false
-		}
-	}
-
-	return true
-}
-
-func isDecreasing(levels []int) bool {
-	for i := 0; i < len(levels)-1; i++ {
-		lvl1, lvl11 := levels[i], levels[i+1]
-		if lvl1 <= lvl11 {
-			return false
-		}
-	}
-
-	return true
-}
-
-func diffIsSave(levels []int) bool {
-	for i := 0; i < len(levels)-1; i++ {
-		lvl1, lvl11 := levels[i], levels[i+1]
-		diff := absDiff(lvl1, lvl11)
-		if diff < 1 || diff > 3 {
-			return false
-		}
-	}
-
-	return true
-}
-
-func absDiff(x, y int) int {
-	return int(math.Abs(float64(x - y)))
 }
