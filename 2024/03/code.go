9bf7dd17eaa291017611b668946d2a640f75f290
Robert Bauernfeind <r.bauernfeind@braun.at>
2024-12-03 12:52:51 +0100

    complete day 3 star 1 | regex

---
+++ 2024/03/code.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"regexp"
+	"strconv"
+
+	"github.com/jpillora/puzzler/harness/aoc"
+)
+
+func main() {
+	aoc.Harness(run)
+}
+
+// on code change, run will be executed 4 times:
+// 1. with: false (part1), and example input
+// 2. with: true (part2), and example input
+// 3. with: false (part1), and user input
+// 4. with: true (part2), and user input
+// the return value of each run is printed to stdout
+func run(part2 bool, input string) any {
+	// when you're ready to do part 2, remove this "not implemented" block
+	if part2 {
+		return "not implemented"
+	}
+	// solve part 1 here
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+
+	// Find all matches
+	matches := re.FindAllStringSubmatch(input, -1)
+
+	sumProd := 0
+	for _, v := range matches {
+		n1, _ := strconv.Atoi(v[1])
+		n2, _ := strconv.Atoi(v[2])
+		sumProd += n1 * n2
+	}
+	return sumProd
+}
